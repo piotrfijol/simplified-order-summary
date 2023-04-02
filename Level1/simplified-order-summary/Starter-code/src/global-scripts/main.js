1742ebc5222ae6d33a7c42bb51120518d326afdb
Piotr Fijol <piotr.fijol1@gmail.com>
2023-04-02 22:43:25 +0200

    Create loading spinner

--- Level1/simplified-order-summary/Starter-code/src/global-scripts/main.js
+++ Level1/simplified-order-summary/Starter-code/src/global-scripts/main.js
@@ -2,4 +2,9 @@
 /// Here we import all the global JavaScript files we need for our project.
 
 import '../global-styles/style.scss'
+import { startLoader } from '../global-scripts/scripts/loading-spinner';
 
+const trigger = document.querySelector(".order-summary__.payment-btn");
+
+trigger
+    .addEventListener('click', startLoader.bind(null, 1500, trigger, trigger));
